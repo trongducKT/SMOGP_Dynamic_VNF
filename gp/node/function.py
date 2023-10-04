fac65a3285ed64b1add1a2fe8f043050cdbfbe6f
trongducKT <buitrongducktmr@gmail.com>
2023-10-04 17:31:02 +0700

    Changing terminal

--- gp/node/function.py
+++ gp/node/function.py
@@ -130,7 +130,7 @@ class PowNode(Node):
 	def GetOutput( self, X ):
 		X0 = self._children[0].GetOutput( X )
 		X1 = self._children[1].GetOutput( X )
-		return np.power(X0, X1)
+		return np.power(X0, 2)
 
 	
 class ExpNode(Node):
